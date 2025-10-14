32a95943e89d02f0a2f49120db88ba8e9cf037ef
1arr4e <jarrueegibide@gmail.com>
2025-10-14 20:18:11 +0200

    Create 01_variables_minireto.py

---
+++ 01_variables_minireto.py
@@ -0,0 +1,15 @@
+# Tenemos un cliente que quiere comprar varios productos.
+# Crea variables para guardar tu nombre, la cantidad de productos y el precio unitario.
+# Calcula el total a pagar.
+# Muestra un mensaje con tu nombre y el total.
+
+nombre = ""      # ¿Cómo llamas al cliente?
+cantidad = 0     # ¿Cuántos productos compra?
+precio = 0       # ¿Precio de un producto?
+
+# Calcula el total
+total = # ???
+
+# Muestra el resultado
+print("Cliente:", nombre)
+print("Total a pagar:", total)
